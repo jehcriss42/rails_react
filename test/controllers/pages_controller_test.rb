ebbeece8cbf97de4426b98aeb7217fe9a66c8dc6
Jessica <jehcriss42@gmail.com>
2020-04-26 23:38:32 -0300

    added tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get 'http://localhost:3000/'
     assert_response :success
   end
 
-  test "should get my_todo_items" do
-    get pages_my_todo_items_url
-    assert_response :success
+  test "should not get my_todo_items" do
+    get 'http://localhost:3000/api/v1/todo_items'
+    assert_response 401
   end
 
 end
