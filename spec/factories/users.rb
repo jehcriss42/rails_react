6283c12dbfa8460702c15d8e20b843bddd8ad2b0
Jessica <jehcriss42@gmail.com>
2020-05-10 22:11:05 -0300

    removed unused code

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,9 +18,9 @@ FactoryBot.define do
         todo_items_count { 5 }
       end
 
-      after(:create) do |user, evaluator|
-        create_list(:completed_todo_item, evaluator.todo_items_count, user: user)
-      end
+      # after(:create) do |user, evaluator|
+      #   create_list(:completed_todo_item, evaluator.todo_items_count, user: user)
+      # end
     end
 
   end
